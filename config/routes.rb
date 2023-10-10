786c77d03e9d1411485696be0ec4c6b007399c8b
Riz <mahmoud.eleresousy@gmail.com>
2023-10-10 12:38:37 +0300

    Add routes folowing instructed details on ERD and wireframes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :users, only:[:index], shallow: true do
+    resources :foods, only: [:index, :create, :new, :destroy]
+    resources :recipes
+  end
+  resources :recipes, shallow: true do
+    resources :recipe_foods, only: [:index, :new, :create, :edit, :update, :destroy]
+    resources :general_shopping_list, only: [:index]
+  end
+  resources :public_recipes, only: [:index]
+  root "users#index"
 end
