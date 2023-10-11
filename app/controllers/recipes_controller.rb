040da12453572e6039f57d7096da317a987bf7f6
Sunga Thawethe <sungabt@gmail.com>
2023-10-11 15:23:28 +0200

    Add Button to New Recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,9 +5,28 @@ class RecipesController < ApplicationController
   end
 
   def new
+    @recipe = Recipe.new
+
+    respond_to do |format|
+      format.html { render :new }
+    end
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user_id = current_user.id
+
+    respond_to do |format|
+      format.html do
+        if @recipe.save
+          flash[:success] = 'Recipe Saved Successfully!'
+          redirect_to recipes_path(user_id: current_user)
+        else
+          flash.now[:error] = 'Error: Post could not be saved'
+          render :new
+        end
+      end
+    end
   end
   
   def show
@@ -20,6 +39,14 @@ class RecipesController < ApplicationController
   end
 
   def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path(user_id: current_user)
   end
 
+  private 
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description, :preparation_time, :cooking_time)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
   def index
-  
+    @user = User.find(params[:id]) 
   end
 end
