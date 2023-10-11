61876a5c05f42dde2f7572f6275d73049e07918b
Sunga Thawethe <sungabt@gmail.com>
2023-10-11 10:40:24 +0200

    Add controllers

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,23 @@
+class RecipesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+  
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :foods, foreign_key: :user_id
   has_many :recipes, foreign_key: :user_id
-
+  
   attribute :name, :string
 
   validates :name, presence: true
