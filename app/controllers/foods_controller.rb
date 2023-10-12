6908fefb38e5dbd67da21598d06749a6e4dc815e
Riz <mahmoud.eleresousy@gmail.com>
2023-10-12 06:54:55 +0300

    Extract foods form in a separate partial and update food new template

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,34 +1,34 @@
 class FoodsController < ApplicationController
-    def index
-        @user = User.find(params[:user_id]) 
-        @foods = @user.foods || []
-    end
+  def index
+    @user = User.find(params[:user_id])
+    @foods = @user.foods || []
+  end
 
-    def new
-        @food = Food.new
-        session[:return_to] ||= request.referer
-    end
+  def new
+    @food = Food.new
+    session[:return_to] ||= request.referer
+  end
 
-    def create
-        user = User.find(params[:user_id])
-        food = user.foods.build(food_params)
-        if food.save
-            redirect_to session.delete(:return_to), notice: "The food was saved successfully!"
-        else
-            flash[:error] = "Could not save the food!"
-            render :new
-        end
+  def create
+    user = User.find(params[:user_id])
+    food = user.foods.build(food_params)
+    if food.save
+      redirect_to session.delete(:return_to), notice: 'The food was saved successfully!'
+    else
+      flash[:error] = 'Could not save the food!'
+      render :new
     end
+  end
 
-    def destroy
-        food = Food.find(params[:id])
-        food.destroy
-        redirect_to user_foods_path(food.user_id)
-    end
+  def destroy
+    food = Food.find(params[:id])
+    food.destroy
+    redirect_to user_foods_path(food.user_id)
+  end
 
-    private
+  private
 
-    def food_params
-        params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
-    end
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
 end
