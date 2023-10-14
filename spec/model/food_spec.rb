40384fd5d000835817d3aaa647f4a8bbd310460c
Sunga Thawethe <sungabt@gmail.com>
2023-10-14 17:28:29 +0200

    Fix linter errors

--- spec/model/food_spec.rb
+++ spec/model/food_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe Food, type: :model do
   subject do
     Food.new(name: 'Flour',
-    measurement_unit: 'grams',
-    price: 3,
-    quantity: 30,
-    user_id: User.create(name: 'Harry').id)
+             measurement_unit: 'grams',
+             price: 3,
+             quantity: 30,
+             user_id: User.create(name: 'Harry').id)
   end
 
   before { subject.save }
--- spec/model/recipe_food_spec.rb
+++ spec/model/recipe_food_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 RSpec.describe RecipeFood, type: :model do
   subject do
     RecipeFood.new(quantity: 30,
-    food_id: Food.new(name: 'Flour',
-    measurement_unit: 'grams',
-    price: 3,
-    quantity: 30,
-    user_id: User.create(name: 'Harry').id).id,
-    recipe_id: Recipe.new(name: 'Lasagna',
-    description: 'Mince, Cheese, Milk, seasoned, baked, italian',
-    preparation_time: 1.5,
-    cooking_time: 2.5,
-    public: true,
-    user_id: User.create(name: 'Harry').id).id)
+                   food_id: Food.new(name: 'Flour',
+                                     measurement_unit: 'grams',
+                                     price: 3,
+                                     quantity: 30,
+                                     user_id: User.create(name: 'Harry').id).id,
+                   recipe_id: Recipe.new(name: 'Lasagna',
+                                         description: 'Mince, Cheese, Milk, seasoned, baked, italian',
+                                         preparation_time: 1.5,
+                                         cooking_time: 2.5,
+                                         public: true,
+                                         user_id: User.create(name: 'Harry').id).id)
   end
 
   before { subject.save }
