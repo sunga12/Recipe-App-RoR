a987114fb7572ac79b91924dabdf296c97b87ead
Riz <mahmoud.eleresousy@gmail.com>
2024-02-03 18:31:09 +0300

    Update user model, devise to not perform confirmation for deployment limitations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable
+        #  :confirmable
 
   # Associations
   has_many :foods, foreign_key: :user_id, dependent: :destroy
